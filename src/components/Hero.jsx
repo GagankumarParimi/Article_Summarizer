905fd1f2e0e3bfe9f704c5cd75c91393b0c0a959
GAGAN KUMAR PARIMI <gagan110302@gmail.com>
2023-07-31 18:09:33 +0530

    second commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/TidbitsJS/Summize", "_blank")
+            window.open("https://github.com/GagankumarParimi/Article_Summarizer", "_blank")
           }
           className='black_btn'
         >
